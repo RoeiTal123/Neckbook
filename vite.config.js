3af1f7d0173d1e5d7addcf7418b40fa94a6ee45f
RoeiTal123 <roeit1977@gmail.com>
2024-01-10 13:22:17 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src', // Adjust the path accordingly
-    },
-  },
   base: '/Neckbook/', // This corresponds to publicPath in Vue CLI
 })
